b6630c0732dd14133fdab31a26c730626df7dfc4
Mohammad Alom <malom1@fordham.edu>
2025-05-13 13:33:06 -0400

    authentication

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -37,11 +38,16 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = ({ storage});
+const upload = multer({ storage});
 
 // ROUTES WITH FILES
 
 app.post("/auth/register", upload.single("picture"), register);
+
+//ROUTES
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes)
 
 // MONGOOSE SETUP
 
